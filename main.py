0c377670777b24704356477ff4f18237b5ab473c
Roed <Geofery10@hotmail.com>
2024-04-01 12:45:17 +0200

    refractored linear_regression_model

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from data import data_cleaner, data_handler, data_visualizer, regression_model
-from data import regression_model
+from data import regression_model, linear_regression_model
 
 # Loads our modified data
 data = data_handler.load_data('data/modified_dataset.csv')
 # data_visualizer.visualize_data(data)
 
-regression_model.train_model(data)
+#regression_model.train_model(data)
+linear_regression_model.train_model(data)
 
 
 def prepare_data():
